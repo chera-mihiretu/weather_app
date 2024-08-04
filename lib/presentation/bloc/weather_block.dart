94c56207042469c296aa5b0b4fd6276e700eb7d8
chera-mihiretu <cheramihiretucm@gmail.com>
2024-08-04 16:46:09 -0400

    Finish testing bloc

--- lib/presentation/bloc/weather_block.dart
+++ lib/presentation/bloc/weather_block.dart
@@ -1,8 +1,19 @@
+import 'package:flutter_basics/domain/usecases/get_weather.dart';
 import 'package:flutter_basics/presentation/bloc/weather_event.dart';
 import 'package:flutter_basics/presentation/bloc/weather_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
-  WeatherBloc(super.initialState);
-
+  final GetWeatherUseCase getWeatherUseCase;
+  WeatherBloc(this.getWeatherUseCase) : super(WeatherEmpty()) {
+    on<GetWeatherEvent>((event, emit) async {
+      emit(WeatherLoading());
+      final result = await getWeatherUseCase.execute();
+      result.fold((failure){
+        emit(WeatherLoadingFailed(failure.message));
+      }, (data){
+        emit(WeatherLoaded(data));
+      });
+    });
+  }
 }
--- test/presentation/bloc/weather_bloc_test.dart
+++ test/presentation/bloc/weather_bloc_test.dart
@@ -1,5 +1,12 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_basics/core/errors/failures/failure.dart';
+import 'package:flutter_basics/domain/entities/weather.dart';
 import 'package:flutter_basics/presentation/bloc/weather_block.dart';
+import 'package:flutter_basics/presentation/bloc/weather_event.dart';
+import 'package:flutter_basics/presentation/bloc/weather_state.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
 import '../../helpers/test_helpers.mocks.dart';
 
@@ -9,10 +16,52 @@ void main (){
 
     setUp((){
       getWeatherUseCase = MockGetWeatherUseCase();
+      weatherBloc = WeatherBloc(getWeatherUseCase);
+    });
+    const myWeather = WeatherEntity(
+      cityName:	"New York",
+      main:	"Snow",
+      description:	"mist",
+      iconCode:	"01d",
+      temperature:	-2.34,
+      pressure:	1001,
+      humidity:	27,
+    );
+
+
+    test('Check if the bloc return WeatherLoading when starts', (){
+
+      expect(weatherBloc.state, WeatherEmpty());
     });
 
+    blocTest<WeatherBloc, WeatherState>('WeatherBloc is working properly',
+        build: (){
+          when(
+            getWeatherUseCase.execute()
+          ).thenAnswer((_) async => const Right(myWeather));
+          return weatherBloc;
+        },
+      act: (bloc) => bloc.add(GetWeatherEvent()),
+      wait: const Duration(milliseconds: 200),
+      expect: () => [
+        WeatherLoading(),
+        const WeatherLoaded(myWeather)
+      ]
+    );
 
-    group('This is for bloc testing', (){
+    blocTest<WeatherBloc, WeatherState>('The block should return error when Error happens',
+        build: (){
+          when(
+            getWeatherUseCase.execute()
+          ).thenAnswer((_) async =>  const Left(ServerFailure('Failed')));
+          return weatherBloc;
+        },
+        act: (bloc) => bloc.add(GetWeatherEvent()),
+        wait: const Duration(milliseconds: 100),
+        expect: () => [
+          WeatherLoading(),
+          const WeatherLoadingFailed('Failed'),
+        ]
+    );
 
-    });
 }
